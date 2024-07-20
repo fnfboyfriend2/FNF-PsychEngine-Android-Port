ca6b929e50466ba6a69f8fe603a677992a7c053b
FNFFrog <135719426+fnfboyfriend2@users.noreply.github.com>
2024-07-21 07:12:13 +0800

    a

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var psychEngineVersion:String = '0.6.3'; //This is also used for Discord RPC
+	public static var psychEngineVersion:String = '0.6.2'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
@@ -156,10 +156,6 @@ class MainMenuState extends MusicBeatState
 		}
 		#end
 
-    #if android
-		addVirtualPad(UP_DOWN, A_B_E);
-    #end
-
 		super.create();
 	}
 
@@ -259,8 +255,8 @@ class MainMenuState extends MusicBeatState
 					});
 				}
 			}
-			#if (desktop || android)
-			else if (FlxG.keys.anyJustPressed(debugKeys) #if android || virtualPad.buttonE.justPressed #end)
+			#if desktop
+			else if (FlxG.keys.anyJustPressed(debugKeys))
 			{
 				selectedSomethin = true;
 				MusicBeatState.switchState(new MasterEditorMenu());
